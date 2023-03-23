ad78cd70ea3b1c9469835ab7c1e8f93e81f2dd96
Gilberto Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-03-22 21:30:01 -0300

    Estado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
-import Button from "./Button";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+    };
+  }
+
   render() {
     return (
-      <>
-        <Button label="Entrar" />
-      </>
+      <main>
+        <h1>{this.state.name}</h1>
+
+        <input
+          value={this.state.name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
+
+        <button onClick={() => console.log("Estado atual: ", this.state.name)}>
+          Mostrar estado
+        </button>
+      </main>
     );
   }
 }
